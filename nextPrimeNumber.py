0e1ce1fa83cdc52db19a362c0c2376453a0be912
ezdimitry <137773973+ezdimitry@users.noreply.github.com>
2024-08-13 15:37:47 +0300

    add nextPrimeNumber

---
+++ nextPrimeNumber.py
@@ -0,0 +1,21 @@
+# нахождение первого простого числа большего числа num
+
+
+def is_prime(num):
+    if num == 1:
+        return False
+    for j in range(2, num):
+        if num % j == 0:
+            return False
+    return True
+
+
+def get_next_prime(num):
+    n = num + 1
+    while not is_prime(n):
+        n += 1
+    return n
+
+
+n = int(input())
+print(get_next_prime(n))
